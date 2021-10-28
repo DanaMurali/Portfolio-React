7ae9bcc138b454e4b2e5232b04d37ee799338ea5
Darania Muralitharan <90543261+DanaMurali@users.noreply.github.com>
2021-10-28 21:40:52 +0100

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="left">
+                <a href="#intro" className = "logo">darania.</a>
+                <div className="itemContainer">
+
+                </div>
+            </div>
+            <div className="right"></div>
+        </div>
+    )
+}
